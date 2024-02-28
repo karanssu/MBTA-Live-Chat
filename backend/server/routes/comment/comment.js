e22eeb83d0f8d9cc8594ad7e1e23918611e45699
karanssu <s0377009salemstate@gmail.com>
2024-02-27 23:50:11 -0500

    Get comment/  finished

--- backend/server/routes/comment/comment.js
+++ backend/server/routes/comment/comment.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const newCommentModel = require("../models/commentModel");
+const newCommentModel = require("../../models/commentModel");
 
-router.get("/getAll", async (req, res) => {
+router.get("/", async (req, res) => {
     const comments = await newCommentModel.find();
     return res.json(comments);
 });
