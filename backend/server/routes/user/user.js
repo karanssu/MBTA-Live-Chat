165b65eaf21837cc766fd55c29b34bddb79d2624
karanssu <s0377009salemstate@gmail.com>
2024-02-27 23:46:07 -0500

    user/getAll changed into user/

--- backend/server/routes/user/user.js
+++ backend/server/routes/user/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const newUserModel = require("../../models/userModel");
 
-router.get("/getAll", async (req, res) => {
+router.get("/", async (req, res) => {
     const user = await newUserModel.find();
     return res.json(user);
 });
