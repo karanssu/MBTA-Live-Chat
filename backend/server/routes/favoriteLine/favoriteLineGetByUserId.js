ce7bac04b514de8f422f3a78d3112cc6cb5edffb
karanssu <s0377009salemstate@gmail.com>
2024-03-07 12:03:11 -0500

    FavirouteLine GetbyuserId and Getbyusername bug fixed

--- backend/server/routes/favoriteLine/favoriteLineGetByUserId.js
+++ backend/server/routes/favoriteLine/favoriteLineGetByUserId.js
@@ -2,17 +2,22 @@ const express = require("express");
 const router = express.Router();
 const favoriteTrainLineModel = require("../../models/favoriteTrainLineModel");
 const userModel = require("../../models/userModel");
-const trainLineModel = require("../../models/trainLineModel");
 
 router.get("/getByUserId/:userId", async (req, res) => {
     const { userId } = req.params;
     const user = await userModel.findById(userId);
-    if (!user) {return res.status(404).send({ message: "UserId not found." });}
+    if (!user) {
+        return res.status(404).send({ message: "UserId not found." });
+    }
 
     try {
-        const trainLines = await trainLineModel.find({ username: user.username });
+        const trainLines = await favoriteTrainLineModel.find({
+            username: user.username,
+        });
         if (trainLines.length === 0) {
-            return res.status(200).send({ message: "User has no Favorite Lines." });
+            return res
+                .status(200)
+                .send({ message: "User has no Favorite Lines." });
         } else {
             return res.json(trainLines);
         }
--- backend/server/routes/favoriteLine/favoriteLineGetByUsername.js
+++ backend/server/routes/favoriteLine/favoriteLineGetByUsername.js
@@ -2,17 +2,22 @@ const express = require("express");
 const router = express.Router();
 const favoriteTrainLineModel = require("../../models/favoriteTrainLineModel");
 const userModel = require("../../models/userModel");
-const trainLineModel = require("../../models/trainLineModel");
 
 router.get("/getByUsername/:username", async (req, res) => {
     const { username } = req.params;
-    const user = await userModel.findOne({ username: username});
-    if (!user) {return res.status(404).send({ message: "Username not found." });}
+    const user = await userModel.findOne({ username: username });
+    if (!user) {
+        return res.status(404).send({ message: "Username not found." });
+    }
 
     try {
-        const trainLines = await trainLineModel.find({ username: user.username });
+        const trainLines = await favoriteTrainLineModel.find({
+            username: user.username,
+        });
         if (trainLines.length === 0) {
-            return res.status(200).send({ message: "User has no Favorite Lines." });
+            return res
+                .status(200)
+                .send({ message: "User has no Favorite Lines." });
         } else {
             return res.json(trainLines);
         }
