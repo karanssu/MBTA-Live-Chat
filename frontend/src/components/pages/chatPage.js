80d8a3fa4154ba994531d46f3dcaaf03fb8b9930
karanssu <s0377009salemstate@gmail.com>
2024-03-28 11:42:29 -0400

    Chat Page is added

---
+++ frontend/src/components/pages/chatPage.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import Card from "react-bootstrap/Card";
+import axios from "axios";
+
+function chat() {
+    return <h1>Chat</h1>;
+}
+
+export default Alerts;
