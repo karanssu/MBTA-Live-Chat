e8d4c3fe339a98ad5fb6264a4526d3c9f0af80e5
karanssu <s0377009salemstate@gmail.com>
2024-04-21 13:55:39 -0400

    trasition speed adjusted

---
+++ backend/server/routes/favoriteLine/favoriteLiineAddByUserId.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const userModel = require("../../models/userModel");
+const favoriteTrainLineModel = require("../../models/favoriteTrainLineModel");
+const trainLineModel = require("../../models/trainLineModel");
+
+router.post("/addByUserId", async (req, res) => {
+    const { userId, trainLine } = req.body;
+
+    const user = await userModel.findById(userId);
+    const trainStation = await trainLineModel.findOne({ trainLine: trainLine});
+    if (!user) return res.status(404).send({ message: "UserId not found." });
+    if (!trainStation) return res.status(404).send({ message: "TrainLine not found." });
+
+    // Check if already in Favorite
+    const inFavorite = await favoriteTrainLineModel.findOne({ username: user.username, trainLine: trainLine });
+    if (inFavorite) return res.status(200).send({ message: "This TrainLine is already your favorite." });
+
+    const addTrainLine = new favoriteTrainLineModel({
+        username: user.username,
+        trainLine: trainLine,
+    });
+
+    try {
+        const saveTrainLine = await addTrainLine.save();
+        res.send(saveTrainLine);
+    } catch (error) {
+        console.log(error);
+        res.status(400).send({ message: "Error trying to add TrainLine." });
+    }
+});
+
+module.exports = router;
