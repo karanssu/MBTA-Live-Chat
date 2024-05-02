d974e6ea15c9825e331ca31e4dc6127a513182ad
huthifa <huthifa001@gmail.com>
2024-05-01 23:24:17 -0400

    Removed Old React Commented out code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -19,28 +19,3 @@ root.render(
     <App />
   </BrowserRouter>
 );
-
-
-// OLD VERSION REACT, What it was before (4/18/24)
-
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import App from "./App";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import { BrowserRouter } from "react-router-dom";
-
-// ReactDOM.render(
-//     // <React.StrictMode>
-
-//     <BrowserRouter>
-//         <link
-//             rel="stylesheet"
-//             href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
-//             integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
-//             crossOrigin="anonymous"
-//         />
-//         <App />
-//     </BrowserRouter>,
-//     // </React.StrictMode>,
-//     document.getElementById("root")
-// );
